d57a621b51e08a257042f0696bedf123e6033322
Pedro Mesquita <91216202+mesquitapj@users.noreply.github.com>
2025-09-13 14:45:50 +0100

    Add files via upload

---
+++ src/main/java/pt/pa/gui/LaptopListCell.java
@@ -0,0 +1,22 @@
+package pt.pa.gui;
+
+import javafx.scene.control.ListCell;
+import pt.pa.model.Laptop;
+
+/**
+ *
+ * @author amfs
+ */
+public class LaptopListCell extends ListCell<Laptop> {
+
+    @Override
+    protected void updateItem(Laptop item, boolean empty) {
+        super.updateItem(item, empty);
+
+        if (empty || item == null) {
+            setText(null);
+        } else {
+            setText(item.getDisplayName());
+        }
+    }
+}
